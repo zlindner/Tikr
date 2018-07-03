d74acbd6dd05822d86ac773e61343f46958b1249
zlindner <zach.lindner@hotmail.com>
2018-07-03 17:11:28 -0400

    started working on layout of site with dummy data

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-    
+    let chart = new Chart($('#stockChart'), {
+        type: 'line',
+        data: {
+            datasets: []
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    display: false,
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        display: false
+                    },
+                    gridLines: {
+                        color: 'transparent'
+                    }
+                }]
+            }
+        }
+    });
 });
