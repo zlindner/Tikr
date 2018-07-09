94064e289cb237cbb4ce2bd1b0d565b02bd30594
zlindner <zach.lindner@hotmail.com>
2018-07-08 22:01:03 -0400

    implemented loggin in + creating account on server, encrypted passwords, db

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
+/* TODO:
+ * 
+ */
+
 $(document).ready(function() {
     $('.form-login input[name="login"]').click(function(e) {
         e.preventDefault();
@@ -7,14 +11,16 @@ $(document).ready(function() {
     $('.form-login input[name="create"]').click(function() {
         $('.form-login').hide();
 
-        $('.wrapper-form').css('height', '360px');
+        $('.form-create-account').trigger('reset');
+        $('.form-error').hide();
         $('.form-create-account').show();
     });
 
     $('.form-login input[name="forgot"]').click(function() {
         $('.form-login').hide();
 
-        $('.wrapper-form').css('height', '280');
+        $('.form-forgot-password').trigger('reset');
+        $('.form-error').hide();
         $('.form-forgot-password').show();
     });
 
@@ -26,40 +32,103 @@ $(document).ready(function() {
     $('.form-create-account input[name="login"]').click(function() {
         $('.form-create-account').hide();
 
-        $('.wrapper-form').css('height', '320px');
+        $('.form-login').trigger('reset');
+        $('.form-error').hide();
         $('.form-login').show();
     });
 
     $('.form-forgot-password input[name="reset"]').click(function(e) {
         e.preventDefault();
-        resetPassword();
+        forgotPassword();
     });
 
     $('.form-forgot-password input[name="login"]').click(function() {
         $('.form-forgot-password').hide();
 
-        $('.wrapper-form').css('height', '320px');
+        $('.form-login').trigger('reset');
+        $('.form-error').hide();
         $('.form-login').show();
     });
 });
 
 function login() {
-    let username = $('.form-login input[name="username"]').val();
-    let password = $('.form-login input[name="password"]').val();
-        
-    console.log('Username: ' + username + '\nPassword: ' + password);
+    let email = $('.form-login input[name="email"]').val();
+    let password = $('.form-login input[name="password"]').val(); 
+
+    if (!validateEmail(email) || password.length == 0) {
+        $('.form-error').text('Check your email and password and try again.');
+        $('.form-error').show();
+        return;
+    }
+
+    $.ajax({
+        type: 'get',
+        url: '/login',
+        dataType: 'json',
+        data: {
+            email: email,
+            password: password
+        },
+        success: function(data) {            
+            if (data.status == 'fail') {
+                $('.form-error').text('Check your email and password and try again.');
+                $('.form-error').show();
+                return;
+            }
+    
+            $('.form-error').hide();
+
+            // login
+        },
+        fail: function(error) {
+            console.log(error);
+        }
+    });
 }
 
 function createAccount() {
-    let email = $('.form-create-account input[name="email"').val();
-    let password = $('.form-create-account input[name="password"').val();
-    let password2 = $('.form-create-account input[name="password2"').val();
+    let email = $('.form-create-account input[name="email"]').val();
+    let password = $('.form-create-account input[name="password"]').val();
+    let password2 = $('.form-create-account input[name="password2"]').val();
 
-    console.log('Email: ' + email + '\nPassword: ' + password + '\nPassword2: '+ password2);
+    if (!validateEmail(email) || password.length == 0 || password2.length == 0 || password != password2) {
+        $('.form-error').text('Check your email and passwords and try again.');
+        $('.form-error').show();
+        return;
+    }
+
+    $('.form-error').hide();
+
+    $.ajax({
+        type: 'get',
+        url: '/createAccount',
+        dataType: 'json',
+        data: {
+            email: email,
+            password: password
+        },
+        success: function(data) {
+
+        },
+        fail: function(error) {
+            console.log(error);
+        }
+    });
 }
 
-function resetPassword() {
-    let email = $('.form-forgot-password input[name="email"').val();
+function forgotPassword() {
+    let email = $('.form-forgot-password input[name="email"]').val();
 
-    console.log('Email: ' + email);
+    if (!validateEmail(email)) {
+        $('.form-error').text('Check your email and try again.');
+        $('.form-error').show();
+        return;
+    }
+
+    $('.form-error').hide();
+}
+
+function validateEmail(email) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
 }
--- server.js
+++ server.js
@@ -1,10 +1,87 @@
 'use strict'
 
+/* TODO:
+ * implement connection pooling
+ * add connection.release()
+ * email verification
+ *     set verified column to true when email is verified
+ */
+
 let express = require('express');
 let http = require('http');
+let mysql = require('mysql');
+let bcrypt = require('bcrypt');
 
 let app = express();
 app.use(express.static('.'));
+
+let db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'no no no',
+    database: 'tikr'
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    
+    console.log('Connected to db');
+});
+
+app.get('/login', function(req, res) {
+    let email = req.query.email;
+    let password = req.query.password;
+
+    // casts binary hash to string
+    db.query('SELECT CAST(password as CHAR) FROM account WHERE email="' + email + '";', function(err, result) {
+        if (err) throw err;
+
+        if (result.length == 0) {
+            // no account with given email
+            return res.json({status: 'fail'});
+        } else {
+            // for some reason json key is 'CAST(password as CHAR)'
+            let hash = result[0]['CAST(password as CHAR)'];
+            
+            bcrypt.compare(password, hash, function(err, match) {
+                if (err) throw err;
+
+                if (match) {
+                    // password was a match
+                    return res.json({status: 'success'});
+                } else {
+                    // password wasn't a match
+                    return res.json({status: 'fail'});
+                }
+            });
+        }
+    });
+});
+
+app.get('/createAccount', function(req, res) {
+    let email = req.query.email;
+    let password = req.query.password;
+
+    db.query('SELECT * FROM account WHERE email="' + email + '";', function(err, result) {
+        if (err) throw err;
+
+        // account doesn't already exist with that email
+        if (result.length == 0) {
+            bcrypt.hash(password, 10, function(err, hash) {
+                if (err) throw err;
+        
+                db.query('INSERT INTO account (email, password) VALUES("' + email + '", "' + hash + '")', function(err, result) {
+                    if (err) throw err;
+
+                    // email verification
+                });
+            });
+        } else {
+            // account already exists with that email
+            return res.json({status: 'fail'});
+        }
+    });
+});
 
 let server = http.createServer(app);
 server.listen(9000);
